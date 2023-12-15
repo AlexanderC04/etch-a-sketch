91f9b11c9c88afb4156a0ef812f337a9aa7749b5
AlexanderC04 <alexandercorderodiaz04@gmail.com>
2023-12-15 19:08:15 -0400

    Create for to create the the div, Gave them some style to see them, and placed on the center of the screen for a better view

---
+++ script.js
@@ -0,0 +1,5 @@
+for (let index = 0; index < 256; index++) {
+  let content = document.createElement("div");
+  content.setAttribute("class", "content");
+  document.querySelector(".container").appendChild(content);
+}
